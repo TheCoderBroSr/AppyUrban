70dd117317f5710674e6415ad99941ae31b8be44
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-03-15 13:08:17 +0530

    adding the file

--- prototype/character_collision.py
+++ prototype/character_collision.py
@@ -10,16 +10,13 @@ clock = pygame.time.Clock()
 screen_width = 900
 screen_height = 600
 screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption("Character Obj - Effects")
+pygame.display.set_caption("Character Obj - Collision")
 
 # Creating the sprites and groups
 G=3
 FPS = 120
 BG = (230, 152, 131)
 player = Player((450, 450), (screen_width, screen_height), G)
-
-moving_sprites = pygame.sprite.Group()
-moving_sprites.add(player)
 
 while True:
     clock.tick(FPS)
