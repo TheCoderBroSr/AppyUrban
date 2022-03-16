b8ff81908336c93db97679e9137bea3c9685b588
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-03-16 16:06:30 +0530

    Player and Obj added, as well as start game line

--- prototype/[test]playground.py
+++ prototype/[test]playground.py
@@ -13,17 +13,28 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Obstacle Obj")
 
-# Creating the sprites and groups
+#Important Game Vars
 G=3
 FPS = 120
 BG = (230, 152, 131)
 BLUE = (5, 23, 61)
+WHITE = (255, 255, 255)
+START_GAME = False
+INTRO_FONT = pygame.font.SysFont('comicsans ms', 30)
 
+text = INTRO_FONT.render("Press space bar to start, or q to quit", 1, WHITE)
+
+#Sprite Groups
+moving_sprites = pygame.sprite.Group()
+collision_sprites = pygame.sprite.Group()
+
+#Initializing player and obstacle objects
+player = Player((screen_width//2, screen_height//2), screen, G)
 obstacle = Obstacle((screen_width, 0), screen, 80, BG, BLUE, G)
 
-collision_sprites = pygame.sprite.Group()
+#Adding them to their respective sprite group
+moving_sprites.add(player)
 collision_sprites.add(obstacle)
-
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -31,11 +42,29 @@ while True:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                START_GAME = True
+
+            if event.key == pygame.K_q:
+                pygame.quit()
+                sys.exit()
+
     screen.fill(BG)
 
-    collision_sprites.draw(screen)
-    collision_sprites.update()
+    if START_GAME:
 
-    obstacle_generation(collision_sprites, 300, screen)
+        #Drawing and updating the sprite groups
+        collision_sprites.draw(screen)
+        collision_sprites.update()
+
+        moving_sprites.update()
+
+        obstacle_generation(collision_sprites, 300, screen)
+        player.move()
+
+    moving_sprites.draw(screen)
+    if not START_GAME:
+        screen.blit(text, (((screen_width - text.get_width())//2),screen_height - 60))
 
     pygame.display.update()
