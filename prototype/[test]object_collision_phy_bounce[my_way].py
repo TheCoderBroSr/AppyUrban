dee944f76f60508e4ab40f9d6a3e61a67d8a99ca
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-03-18 16:21:10 +0530

    Code can generate new random simulations

--- prototype/[test]object_collision_phy_bounce[my_way].py
+++ prototype/[test]object_collision_phy_bounce[my_way].py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, random, sys
 
 # General setup
 pygame.init()
@@ -12,11 +12,18 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Character Collision Bounce - Physics")
 
 # Some important vars
+TITTLE_FONT = pygame.font.SysFont('Cooper Black', 30)
 FPS = 120
+BG = (230, 152, 131)
+BLUE = (5, 23, 61)
+testcase_no = 1
 G = 1 #Is the acceleration
 
 Gx = 2*G
 Gy = G
+START = False
+
+title = TITTLE_FONT.render("Press 'p' to start the simulation, and 'q' to stop", 1, BLUE)
 
 obstacle = pygame.Rect(screen_width - 300, 0, 60, screen_height)
 player_dummy = pygame.Rect(100, screen_height - 150, 50, 50)
@@ -25,35 +32,60 @@ collided = 0
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type==pygame.KEYDOWN and event.key == pygame.K_q):
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    screen.fill((255, 255, 255))
+        if event.type==pygame.KEYDOWN: 
+            if event.key == pygame.K_q:
+                pygame.quit()
+                sys.exit()
 
-    pygame.draw.rect(screen, (0,0,0), obstacle)
-    pygame.draw.rect(screen, (20, 50, 90), player_dummy)
+            if event.key == pygame.K_p:
+                START = True
 
-    #Move the player
-    player_dummy.x += Gx #The accelerations
-    player_dummy.y -= Gy
+    screen.fill(BG)
 
-    #Checking if dummy player has collided
-    if player_dummy.x + player_dummy.width >= obstacle.x:
-        Gx *= -1
-        collided = 1
+    if not START:
+        screen.blit(title, ((screen_width - title.get_width())//2, screen_height//2))
 
-    if collided:
-        if Gx <= 0 and Gy >= 0:
-            Gx += 0.02
-            Gy -= 0.0099
+    if START:
+        pygame.draw.rect(screen, (0,0,0), obstacle)
+        pygame.draw.rect(screen, (20, 50, 90), player_dummy)
 
-        if Gy < 0:
-            Gy = 0
+        testcase = TITTLE_FONT.render(f"Case No. {testcase_no}", 1, BLUE)
+        screen.blit(testcase, ((screen_width - testcase.get_width() - 30), 10))
 
-        if Gx > 0:
-            Gx = 0
+        #Move the player
+        #The accelerations are Gx & Gy
+        player_dummy.x += Gx
+        player_dummy.y -= Gy
 
-    print(Gx, Gy)
+        #Checking if dummy player has collided
+        if player_dummy.x + player_dummy.width >= obstacle.x:
+            Gx *= -1
+            collided = 1
+
+        if collided:
+            if Gx <= 0 and Gy >= 0:
+                Gx += 0.02
+                Gy -= 0.0099
+
+            if Gy < 0:
+                Gy = 0
+
+            if Gx > 0:
+                Gx = 0
+
+        if Gx == 0 and Gy == 0:
+            pygame.time.delay(1000)
+            
+            Gx = 2*G
+            Gy = G
+            collided = 0
+            testcase_no += 1
+
+            player_dummy.x = random.randint(10, screen_width - 400)
+            player_dummy.y = random.randint(500, screen_height - 100)
 
     pygame.display.update()
