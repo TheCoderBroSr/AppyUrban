37d9c4d8991cc7e7a1d7f09e7df92bed75a767de
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-03-18 21:38:07 +0530

    Glitchy Random generated obstacles & collisions

--- prototype/[test]object_collision_phy[my_version].py
+++ prototype/[test]object_collision_phy[my_version].py
@@ -17,10 +17,8 @@ FPS = 120
 BG = (230, 152, 131)
 BLUE = (5, 23, 61)
 testcase_no = 1
-G = 1 #Is the acceleration
+G = 3 #Is the acceleration
 
-Gx = 2*G
-Gy = G
 START = False
 
 title = TITTLE_FONT.render("Press 'p' to start the simulation, and 'q' to stop", 1, BLUE)
@@ -35,6 +33,9 @@ reset_counter = 0
 
 initial_player_x = player_dummy.x
 initial_player_y = player_dummy.y
+
+target_y = random.randint(obstacle.y, obstacle.y + obstacle.height)
+target_x = random.randint(obstacle.x, obstacle.x + obstacle.width)
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -62,22 +63,38 @@ while True:
         testcase = TITTLE_FONT.render(f"Case No. {testcase_no}", 1, BLUE)
         screen.blit(testcase, ((screen_width - testcase.get_width() - 30), 10))
 
-        #Move the player
-        #The accelerations are Gx & Gy
-        if initial_player_x < screen_width//2:
+        #Determining the Gx and Gy
+        # (target_y - obstacle.y)^2 + (Gx)^2 = (D)^2
+        D = ((target_x - obstacle.x)**2 + (target_y - obstacle.y)**2)**0.5 #Euclidean Distance
+        Gx = (D**2 - (target_y - obstacle.y)**2)**0.5 #By Pythagoras Theoram
+        Gy = (D**2 - Gx**2)**0.5
+
+        while Gx > G:
+            Gx //= 2
+
+        while Gy > G:
+            Gy //= 2
+
+        #Determine the direction
+        if initial_player_x < obstacle.x//2:
             player_dummy.x += Gx
         else:
             player_dummy.x -= Gx
 
-        if initial_player_y > screen_height//2:
+        if initial_player_y > obstacle.y//2:
             player_dummy.y -= Gy
         else:
             player_dummy.y += Gy
 
         #Checking if dummy player has collided
         if player_dummy.colliderect(obstacle):
-            Gx *= -1
+            if obstacle.height > obstacle.width: #Checking if obstacle is vertical
+                Gx *= -1
+            else:
+                Gy *= -1
             collided = 1
+
+        print(Gx, Gy)
 
         if collided == 1:
             reset_counter += 1
@@ -88,15 +105,33 @@ while True:
             collided = 0
             testcase_no += 1
 
+            #Randomly generating AND placing the obstacle
+            obstacle.x = random.randint(20, screen_width - 20)
+            obstacle.y = random.randint(20, screen_height - 20)
+            obstacle.width = random.randint(20, screen_width)
+            obstacle.height = random.randint(20, screen_height)
+
+            while obstacle.x not in range(screen_width) and (obstacle.x + obstacle.width) not in range(screen_width) and obstacle.y not in range(screen_height) and (obstacle.y + obstacle.height) not in range(screen_height):
+                obstacle.x = random.randint(20, screen_width - 20)
+                obstacle.y = random.randint(20, screen_height - 20)
+                obstacle.width = random.randint(20, screen_width)
+                obstacle.height = random.randint(20, screen_height)
+
+            #Randomly placing the player
             player_dummy.x = random.randint(10, screen_width - 75)
             player_dummy.y = random.randint(10, screen_height - 100)
 
             #Checking for dead zones
-            while player_dummy.x in range(obstacle.x - player_dummy.width, obstacle.x + obstacle.width + player_dummy.width):
+            while player_dummy.colliderect(obstacle):
                 player_dummy.x = random.randint(10, screen_width - 75)
+                player_dummy.y = random.randint(10, screen_height - 100)
 
             initial_player_x = player_dummy.x
             initial_player_y = player_dummy.y
+
+            D = ((target_x - obstacle.x)**2 + (target_y - obstacle.y)**2)**0.5 #Euclidean Distance
+            Gx = (D**2 - (target_y - obstacle.y)**2)**0.5 #By Pythagoras Theoram
+            Gy = (D**2 - Gx**2)**0.5
 
             #Resetting the vars
             collided = 0
