1c5ef64df92394f546db332c91da41ec5857aebf
TheCoderBroSr <arhantsurajkumar@gmail.com>
2022-03-19 16:27:30 +0530

    Underdevelopment speed determiner

--- prototype/[test]object_collision_phy[my_version].py
+++ prototype/[test]object_collision_phy[my_version].py
@@ -65,25 +65,32 @@ def generate_target_point(obstacle):
 
     return target_point
 
+def determine_speed(player, obstacle, target_point, time=40):
+    #Euclidean Distance
+    distance_obs_to_target = ((target_point.x - obstacle.x)**2 + (target_point.y - obstacle.y)**2)**0.5
+    slant_distance = ((player.x - target_point.x)**2 + (player.y - target_point.y)**2)**0.5
+
+    #Using pythagoras theoram
+    Dx = (slant_distance**2 - distance_obs_to_target**2)**0.5 #horizontal distance
+    Dy = (slant_distance**2 - Dx**2)**0.5 #vertical distance
+
+    #speeds
+    Vx = Dx/time
+    Vy = Dy/time
+
+    return (Vx, Vy)
+
 obstacle, player_dummy = spawn(20)
 
 collided = 0
 reset_counter = 0
 
-initial_player_x = player_dummy.x
-initial_player_y = player_dummy.y
-
+initial_player_x, initial_player_y = player_dummy.topleft
 target_x, target_y = generate_target_point(obstacle)
+
 #Determining the Gx and Gy
-D = ((target_x - obstacle.x)**2 + (target_y - obstacle.y)**2)**0.5 #Euclidean Distance
-Gx = (D**2 - (target_y - obstacle.y)**2)**0.5 #By Pythagoras Theoram
-Gy = (D**2 - Gx**2)**0.5
+Gx, Gy = determine_speed(player_dummy, obstacle, (target_x, target_y))
 
-while Gx > 3:
-    Gx //= 2
-
-while Gy > 3:
-    Gy //= 2
 while True:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -108,24 +115,30 @@ while True:
         screen.blit(title, ((screen_width - title.get_width())//2, screen_height//2))
 
     if START:
+        #Drawing the obstacle and player
         pygame.draw.rect(screen, (0,0,0), obstacle)
         pygame.draw.rect(screen, (20, 50, 90), player_dummy)
 
-        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(target_x, target_y, 10, 10))
+        #Target for the player
+        pygame.draw.circle(screen, (255, 0, 0), (target_x, target_y), 5, 2)
+
+        #The Path
+        pygame.draw.line(screen, (134,56,105), (player_dummy.x, player_dummy.y), (target_x, target_y), width=2)
+
 
         testcase = TITTLE_FONT.render(f"Case No. {testcase_no}", 1, BLUE)
         screen.blit(testcase, ((screen_width - testcase.get_width() - 30), 10))
 
         #Determine the direction
-        # if initial_player_x < obstacle.x//2:
-        #     player_dummy.x += Gx
-        # else:
-        #     player_dummy.x -= Gx
+        if initial_player_x < obstacle.x//2:
+            player_dummy.x += Gx
+        else:
+            player_dummy.x -= Gx
 
-        # if initial_player_y > obstacle.y//2:
-        #     player_dummy.y -= Gy
-        # else:
-        #     player_dummy.y += Gy
+        if initial_player_y > obstacle.y//2:
+            player_dummy.y -= Gy
+        else:
+            player_dummy.y += Gy
 
         #Checking if dummy player has collided
         if player_dummy.colliderect(obstacle):
@@ -138,16 +151,14 @@ while True:
         if collided == 1:
             reset_counter += 1
 
-        if reset_counter == 20:
+        if reset_counter == FPS:
             collided = 0
             testcase_no += 1
 
             #Randomly generating AND placing the obstacle
             obstacle, player_dummy = spawn(20)
 
-            initial_player_x = player_dummy.x
-            initial_player_y = player_dummy.y
-
+            initial_player_x, initial_player_y = player_dummy.topleft
             target_x, target_y = generate_target_point(obstacle)
 
             D = ((target_x - obstacle.x)**2 + (target_y - obstacle.y)**2)**0.5 #Euclidean Distance
